f81e51d4057e1efa28af2b613a7b64ccb2686972
aharoot2015 <aharoot2015@gmail.com>
2017-01-10 21:02:53 +0800

    first commit

---
+++ authentication/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Mvc;
+using System.Web.Security;
+
+namespace authentication.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost] // 未加註記，會產生無法判斷要處理哪一個ActionResult
+        public ActionResult Index(string name)
+        {
+           
+            if (name == "123")
+            {
+                FormsAuthentication.SetAuthCookie(name, false);
+              
+                return RedirectToAction("秘密");
+               
+            }
+            return View();
+        }
+        [Authorize]
+        public ActionResult 秘密()
+        {
+           FormsAuthentication.SignOut();
+            return View();
+        }
+    }
+}
